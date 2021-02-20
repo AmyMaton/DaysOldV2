741dc8ed2387be2f13d49da7d540d64615977cfd
AmyMaton <57409803+AmyMaton@users.noreply.github.com>
2021-02-20 05:05:02 +0000

    Basic Movment

---
+++ DaysOldV2/Assets/Scripts/PlayerMovcment.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovcment : MonoBehaviour
+{
+    public float MovementSpeed = 10.0f;
+    public Rigidbody2D Rigidbody;
+    private Vector2 MovementDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputProcessing();
+    }
+
+    void FixedUpdate()
+    {
+        Movement();   
+    }
+
+    void InputProcessing()
+    {
+        float MoveX = Input.GetAxisRaw("Horizontal");
+        float MoveY = Input.GetAxisRaw("Vertical");
+
+        MovementDirection = new Vector2(MoveX, MoveY).normalized;
+    }
+
+    void Movement()
+    {
+        Rigidbody.velocity = new Vector2(MovementDirection.x * MovementSpeed, MovementDirection.y * MovementSpeed);
+    }
+}
