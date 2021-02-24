b77a7c00d43e56a52cdcae11182041ed3c5680ef
AmyMaton <57409803+AmyMaton@users.noreply.github.com>
2021-02-24 05:26:58 +0000

    Added Tree and Rock Collision + 3 rock levels dependent on "Aridness"
    yes i chose the word Arid for the ludwig meme

---
+++ DaysOldV2/Assets/Scripts/Inventiory.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventiory : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static int Collums = 6;
+    public static int Rows = 3;
+
+    public int[,] InventoryArray = new int[Rows,Collums];
+
+    void Start()
+    {
+        
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Collums; j++)
+            {
+                InventoryArray[i, j] = 0;
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Collums; j++)
+                {
+                    InventoryArray[i, j] = 1;
+                }
+            }
+        }
+    }
+}
---
+++ DaysOldV2/Assets/Scripts/RockScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public SpriteRenderer spriteRenderer;
+
+    public int Aridness = 50;
+
+    public Sprite NormalRock;
+    public Sprite DryRock;
+    public Sprite WetRock;
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+        if (Aridness < 25)
+        {
+            spriteRenderer.sprite = WetRock;
+        }
+        else if (Aridness > 75)
+        {
+            spriteRenderer.sprite = DryRock;
+        }
+        else
+        {
+            spriteRenderer.sprite = NormalRock;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+     
+    }
+}
